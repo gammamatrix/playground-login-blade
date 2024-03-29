af904b26b4893f9723a1f91296f7c61fdfa40de1
Jeremy Postlethwaite <jeremy.postlethwaite@gmail.com>
2024-03-28 22:45:09 -0700

    v73.0.0

--- routes/token.php
+++ routes/token.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 
 Route::group([
-    'middleware' => config('playground-login-blade.middleware.guest'),
+    'middleware' => config('playground-login-blade.middleware.default'),
     'namespace' => '\Playground\Login\Blade\Http\Controllers',
 ], function () {
     Route::get('/token', [
